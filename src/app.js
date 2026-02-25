b1997662fd4435a145af5fa1b78fc738f76f2b90
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2026-02-24 22:16:26 -0300

    cargando modificacion en app para poder arrancar server y hacer pruebas en postman

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 
-
+import productsRouter from "./routes/products.routes.js";
 
 const app = express();
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
-app.use(morgan("dev")); 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
+app.use("/api/products", productsRouter);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ ok: true });
-});
+// test rapido
+app.get("/", (req,res)=> res.send("API funcionando"));
 
 export default app;
