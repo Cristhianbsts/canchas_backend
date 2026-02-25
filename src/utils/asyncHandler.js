b64c5d8d58af5eb125f50e86aed3e11a4d10af0e
rubendevlop <71162403+rubendevlop@users.noreply.github.com>
2026-02-24 21:29:49 -0300

    Update asyncHandler.js
    quitando comentarios porque no son necesarios"

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,3 @@
-// src/utils/asyncHandler.js
-// Wrapper para evitar try/catch en controllers.
-// Cualquier error async se manda al error middleware con next(err)
-
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
