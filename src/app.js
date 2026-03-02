7a8cc5db97fbb2d2f875b5e374a63f44413fbff0
Martin <marticicarodriguez@gmail.com>
2026-03-01 22:00:26 -0300

    hemos creado un controlador book.controller una ruta route.Booking y hemos realizado una modificacion del modelo de Book y hemos creado un buscador de los horarios disponibles para cancha y dia y una funcion para guardar una reserva

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js"
-import cookieParser from "cookie-parser";
+//import cookieParser from "cookie-parser.js";
 import productsRouter from "./routes/products.routes.js";
 import categoriesRouter from "./routes/categories.routes.js";
 import ruteRegister from './rutes/ruteRegister.js'
+import routeBooking from "../src/rutes/routeBooking.js"
 
 
 
@@ -14,13 +15,15 @@ const app = express();
 app.use(morgan("dev")); 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
-app.use(cookieParser())
+//app.use(cookieParser())
 app.use(cors());
 
 app.use("/api/products", productsRouter);
 app.use("/api/categories", categoriesRouter);
 app.use('/api/auth', ruteRegister )
 app.use("/api/auth",authRoutes)
+app.use("/api/findbook",routeBooking)
+
 
 
 
