57d5c7f0599ba5282929b42bd7f154d7857fe79b
Facundo Vera <facundovera.dev@gmail.com>
2026-03-03 14:44:36 -0300

    Importo verifyToken y corrijo errores de tipeo en middleware de autenticación

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,9 @@ import { Router } from "express";
 import { validateLoginUser } from "../middlewares/validator.middleware.js";
 import { login } from "../controller/auth.controller.js";
 
+
 const router = Router();
 
-
-
-router.post("/login",validateLoginUser(),login)
-
+router.post("/login", validateLoginUser(), login);
 
 export default router;
